b51a6d7803f5b8d3b55f8bd7abbb48066087906a
reimiii <imiia75775@gmail.com>
2022-03-20 21:17:32 +0700

    litle

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-console.log('server....');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).json({ pesan: 'Hello ini dari backend' });
+});
+
+app.listen(PORT, () => console.log(`Server diPort ${PORT}`));
